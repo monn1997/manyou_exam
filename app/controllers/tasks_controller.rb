516551251ff64a7b15c8d608e9cf239b345fc973
tomomi adachi <tomoni1997@gmai.com>
2023-02-02 01:25:19 +0900

    削除項目の実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]  
+
   def index
     @tasks = Task.all
   end  
@@ -34,9 +36,18 @@ class TasksController < ApplicationController
     end    
   end
 
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, notice:"投稿を削除しました！"
+  end
+
   private
 
   def task_params
     params.require(:task).permit(:title, :content)
+  end  
+
+  def set_task
+    @task = Task.find(params[:id])
   end  
 end
