20edb63da18f3cf198b433e379cce86561059a41
tomomi adachi <tomoni1997@gmai.com>
2023-02-07 01:03:45 +0900

    タスク一覧を作成日時の順番で並び替える

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -6,27 +6,40 @@ RSpec.describe 'タスク管理機能', type: :system do
         task = FactoryBot.create(:task)
         visit new_task_path
         fill_in 'task[content]', with: 'task'
-        sleep(3)
+        fill_in 'task[title]', with: 'task'
+        sleep(5)
         click_on 'Create Task'
         expect(page).to have_content 'task'
       end
     end
-  end
-  describe '一覧表示機能' do
-    context '一覧画面に遷移した場合' do
-      it '作成済みのタスク一覧が表示される' do
-        # テストで使用するためのタスクを作成
-        task = FactoryBot.create(:task)
-        # タスク一覧ページに遷移
-        visit tasks_path
-        # visitした（遷移した）page（タスク一覧ページ）に「task」という文字列が
-        # have_contentされているか（含まれているか）ということをexpectする（確認・期待する）
-        expect(page).to have_content 'タスク1'
-        # expectの結果が true ならテスト成功、false なら失敗として結果が出力される
+  end  
+
+    describe '一覧表示機能' do
+      context '一覧画面に遷移した場合' do
+        it '作成済みのタスク一覧が表示される' do
+          # テストで使用するためのタスクを作成
+          task = FactoryBot.create(:task)
+          # タスク一覧ページに遷移
+          visit tasks_path
+          # visitした（遷移した）page（タスク一覧ページ）に「task」という文字列が
+          # have_contentされているか（含まれているか）ということをexpectする（確認・期待する）
+          expect(page).to have_content 'タスク1'
+          # expectの結果が true ならテスト成功、false なら失敗として結果が出力される
+        end
       end
     end
-  end
-  describe '詳細表示機能' do
+
+    context 'タスクが作成日時の降順に並んでいる場合' do
+      it '新しいタスクが一番上に表示される' do
+        task = FactoryBot.create(:task)
+        visit tasks_path
+        task_list = all('.task_row')
+        sleep(5)
+      end
+    end
+
+
+    describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示される' do
          task = FactoryBot.create(:task)
