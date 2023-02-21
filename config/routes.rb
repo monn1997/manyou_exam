ed6a7113d5e94adadf533bfece5d0ce030cf13ed
tomomi adachi <tomoni1997@gmai.com>
2023-02-21 12:08:41 +0900

    ログイン機能実装・マイグレーションファイル修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
-  resources :tasks do
+  resources :tasks do  
     collection do
       post :confirm
     end
+  end
+
+  resources :users, only: [:new, :create, :index, :show]
   end    
-end
+
