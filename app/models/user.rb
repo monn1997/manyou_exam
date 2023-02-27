bc90cb82dd3f3b86c676de4b2fb300e0912d7284
tomomi adachi <tomoni1997@gmai.com>
2023-02-27 19:37:43 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  before_update :admin_update_exist
+  before_destroy :admin_exist_check
+
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 }, 
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i } 
@@ -7,7 +10,23 @@ class User < ApplicationRecord
   has_secure_password   
   validates :password, length: { minimum: 6 }  
   has_many :tasks, dependent: :destroy          
+
+private
+
+def admin_update_exist
+  if User.where(admin: true).count == 1 && self.admin == false
+    throw(:abort)
+  end
 end
+
+def admin_exist_check
+  if User.where(admin: true).count <= 1 && self.admin == true
+    throw(:abort)
+  end
+end
+
+end
+
 
 
 
