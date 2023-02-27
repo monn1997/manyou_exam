efe53062dcfb0350e3bf75d63bc2d075c3163d99
tomomi adachi <tomoni1997@gmai.com>
2023-02-27 14:57:04 +0900

    テストの実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
+
   factory :user do
-    
+    name { "normaluser" }
+    email { "normal@gmail.com" }
+    password { "111111" }
+    password_confirmation { "111111" }
+    admin {false}
+  end
+
+  factory :admin_user, class: User do
+    name { "adminuser" }
+    email { "admin@gmail.com" }
+    password { "222222" }
+    password_confirmation { "222222" }
+    admin {true}
   end
 end
